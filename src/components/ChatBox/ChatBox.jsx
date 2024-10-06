f27cf0fe390048841a0337a003cb24cc15706021
Hind_Jammar <121793980+hindjammar@users.noreply.github.com>
2024-10-06 23:28:58 +0100

    New Login Page

---
+++ src/components/ChatBox/ChatBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './ChatBox.css'
+import assets from '../../assets/assets'
+
+
+const ChatBox = () => {
+  return (
+    <div className='chat-box'>
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="" />
+        <p>Alami Yahya <img src={assets.green_dot} alt="" /></p>
+        <img src={assets.help_icon} className='help' alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default ChatBox
--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Chat.css'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
+import ChatBox from '../../components/ChatBox/ChatBox'
 
 const Chat = () => {
   return (
-    <div>
+    <div className='chat'>
+            <div className="chat-container">
+                <LeftSidebar/>
+                <ChatBox/>
+                <RightSidebar/>
 
+            </div>
     </div>
   )
 }
